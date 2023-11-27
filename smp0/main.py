542ca8b4a50070a708dc3bc35a0776eabd5d0d93
Marco <97095997+mnlmrc@users.noreply.github.com>
2023-11-27 09:35:19 -0500

    update

--- smp0/main.py
+++ smp0/main.py
@@ -2,98 +2,111 @@ import matplotlib
 
 matplotlib.use('MacOSX')
 
-import matplotlib.patches as mpatches
-import matplotlib.pyplot as plt
-import numpy as np
-
-from smp0.util import sort_by_probability, average_within_timewin, subtract_baseline
+from smp0.util import experiment_dataframe
 
 experiment = 'smp0'
-participant_id = '100'
-# block = '01'
+participants_id = ['100']
 
-cues = ['25%', '50%', '75%', '100%']
-timewins = ['25-50 ms', '50-200 ms', '100-500 ms']
-stimFingers = ['index', 'ring']
-allFingers = ['thumb', 'index', 'middle', 'ring', 'pinkie']
+process = input('input process')
 
-probCues = (sort_by_probability(experiment, participant_id, 91999),
-            sort_by_probability(experiment, participant_id, 99919))
-probCues = subtract_baseline(probCues)
-tAx = probCues[0][1]
+match process:
 
-meanTimewin = (average_within_timewin(probCues, [.025, .05], pre_stim_time=1, fsample=500),
-               average_within_timewin(probCues, [.05, .2], pre_stim_time=1, fsample=500),
-               average_within_timewin(probCues, [.1, .5], pre_stim_time=1, fsample=500))
 
-fResp_mean = np.zeros((len(meanTimewin), len(stimFingers), len(cues), len(allFingers)))
-fResp_sd = np.zeros((len(meanTimewin), len(stimFingers), len(cues), len(allFingers)))
-for timewin in range(len(meanTimewin)):
-    for finger in range(len(stimFingers)):
-        for cue in range(len(cues)):
-            fResp_mean[timewin, finger, cue, :] = meanTimewin[timewin][finger][cue].mean(axis=0)
-            fResp_sd[timewin, finger, cue, :] = meanTimewin[timewin][finger][cue].std(axis=0)
+    case 'create dataframe':
+        data = experiment_dataframe(experiment, participants_id)
 
-fig1, axs1 = plt.subplots(4, 2, sharey=True, sharex=True, figsize=(6, 7))
+    case _:
+        print('command not recognized')
 
-ylim = [-5, 30]
-xlim = [-.5, 1]
-lw = .5
 
-for cue in range(len(cues)):
-    for finger in range(len(stimFingers)):
 
-        axs1[cue, finger].plot(tAx, np.array(probCues[finger][0][cue]).mean(axis=0))
 
-        axs1[cue, finger].set_xlim(xlim)
-        axs1[cue, finger].set_ylim(ylim)
 
-        axs1[cue, finger].vlines(0, ylim[1], ylim[0], ls='-', color='k', lw=lw)
-        axs1[cue, finger].vlines(.025, ylim[1], ylim[0], ls='--', color='k', lw=lw)
-        axs1[cue, finger].vlines(.05, ylim[1], ylim[0], ls='--', color='k', lw=lw)
-        axs1[cue, finger].vlines(.2, ylim[1], ylim[0], ls='--', color='k', lw=lw)
-        axs1[cue, finger].vlines(.5, ylim[1], ylim[0], ls='--', color='k', lw=lw)
-        axs1[cue, finger].hlines(0, xlim[0], xlim[1], ls='-', color='k', lw=lw)
-
-        axs1[cue, finger].set_title(stimFingers[finger] + ', ' + cues[cue])
-
-fig1.legend(allFingers, loc='upper center', bbox_to_anchor=(.5, .975), ncol=5)
-
-fig1.supylabel('force (N)')
-fig1.supxlabel('time (s)')
-fig1.subplots_adjust(hspace=.4, right=.96, left=.1, bottom=.09)
-fig1.show()
-
-fig2, axs2 = plt.subplots(3, 2, sharex=True, sharey='row', figsize=(6, 7))
-
-xscatter = np.linspace(-.2, .2, len(allFingers))
-xoffset = np.linspace(0, len(cues) - 1, len(cues))
-barcolors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd']
-barwidth = np.diff(xscatter).mean()
-
-for timewin in range(len(meanTimewin)):
-    for finger in range(len(stimFingers)):
-        for cue in range(len(cues)):
-            axs2[timewin, finger].bar(xscatter + xoffset[cue], fResp_mean[timewin, finger, cue, :],
-                                      color=barcolors, width=barwidth, yerr=fResp_sd[timewin, finger, cue, :])
-            axs2[timewin, finger].set_title(stimFingers[finger] + ', ' + timewins[timewin])
-            axs2[timewin, finger].set_xlim([-.5, 3.5])
-
-            if cue == 0:
-                axs2[timewin, finger].hlines(0, axs2[timewin, finger].get_xlim()[0],
-                                             axs2[timewin, finger].get_xlim()[1], lw=lw, color='k')
+# participant_id = '100'
+# # block = '01'
 #
-# for c, timewin in enumerate(timewins):
-#     axs2[c, 0].text(1, 11, timewin)
-
-axs2[-1, 0].set_xticks(np.linspace(0, 3, 4))
-axs2[-1, 0].set_xticklabels(cues)
-
-handles = [mpatches.Patch(color=color) for color in barcolors]
-fig2.legend(handles=handles, labels=allFingers, loc='upper center', bbox_to_anchor=(.5, 1), ncol=5)
-
-fig2.supylabel('force (N)')
-fig2.supxlabel('cued probability')
-# fig2.tight_layout()
-fig2.subplots_adjust(hspace=.4, right=.96, left=.1, bottom=.085, top=.9)
-fig2.show()
+# cues = ['25%', '50%', '75%', '100%']
+# timewins = ['25-50 ms', '50-200 ms', '100-500 ms']
+# stimFingers = ['index', 'ring']
+# allFingers = ['thumb', 'index', 'middle', 'ring', 'pinkie']
+#
+# probCues = (sort_by_probability(experiment, participant_id, 91999),
+#             sort_by_probability(experiment, participant_id, 99919))
+# probCues = subtract_baseline(probCues)
+# tAx = probCues[0][1]
+#
+# meanTimewin = (average_within_timewin(probCues, [.025, .05], pre_stim_time=1, fsample=500),
+#                average_within_timewin(probCues, [.05, .2], pre_stim_time=1, fsample=500),
+#                average_within_timewin(probCues, [.1, .5], pre_stim_time=1, fsample=500))
+#
+# fResp_mean = np.zeros((len(meanTimewin), len(stimFingers), len(cues), len(allFingers)))
+# fResp_sd = np.zeros((len(meanTimewin), len(stimFingers), len(cues), len(allFingers)))
+# for timewin in range(len(meanTimewin)):
+#     for finger in range(len(stimFingers)):
+#         for cue in range(len(cues)):
+#             fResp_mean[timewin, finger, cue, :] = meanTimewin[timewin][finger][cue].mean(axis=0)
+#             fResp_sd[timewin, finger, cue, :] = meanTimewin[timewin][finger][cue].std(axis=0)
+#
+# fig1, axs1 = plt.subplots(4, 2, sharey=True, sharex=True, figsize=(6, 7))
+#
+# ylim = [-5, 30]
+# xlim = [-.5, 1]
+# lw = .5
+#
+# for cue in range(len(cues)):
+#     for finger in range(len(stimFingers)):
+#
+#         axs1[cue, finger].plot(tAx, np.array(probCues[finger][0][cue]).mean(axis=0))
+#
+#         axs1[cue, finger].set_xlim(xlim)
+#         axs1[cue, finger].set_ylim(ylim)
+#
+#         axs1[cue, finger].vlines(0, ylim[1], ylim[0], ls='-', color='k', lw=lw)
+#         axs1[cue, finger].vlines(.025, ylim[1], ylim[0], ls='--', color='k', lw=lw)
+#         axs1[cue, finger].vlines(.05, ylim[1], ylim[0], ls='--', color='k', lw=lw)
+#         axs1[cue, finger].vlines(.2, ylim[1], ylim[0], ls='--', color='k', lw=lw)
+#         axs1[cue, finger].vlines(.5, ylim[1], ylim[0], ls='--', color='k', lw=lw)
+#         axs1[cue, finger].hlines(0, xlim[0], xlim[1], ls='-', color='k', lw=lw)
+#
+#         axs1[cue, finger].set_title(stimFingers[finger] + ', ' + cues[cue])
+#
+# fig1.legend(allFingers, loc='upper center', bbox_to_anchor=(.5, .975), ncol=5)
+#
+# fig1.supylabel('force (N)')
+# fig1.supxlabel('time (s)')
+# fig1.subplots_adjust(hspace=.4, right=.96, left=.1, bottom=.09)
+# fig1.show()
+#
+# fig2, axs2 = plt.subplots(3, 2, sharex=True, sharey='row', figsize=(6, 7))
+#
+# xscatter = np.linspace(-.2, .2, len(allFingers))
+# xoffset = np.linspace(0, len(cues) - 1, len(cues))
+# barcolors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd']
+# barwidth = np.diff(xscatter).mean()
+#
+# for timewin in range(len(meanTimewin)):
+#     for finger in range(len(stimFingers)):
+#         for cue in range(len(cues)):
+#             axs2[timewin, finger].bar(xscatter + xoffset[cue], fResp_mean[timewin, finger, cue, :],
+#                                       color=barcolors, width=barwidth, yerr=fResp_sd[timewin, finger, cue, :])
+#             axs2[timewin, finger].set_title(stimFingers[finger] + ', ' + timewins[timewin])
+#             axs2[timewin, finger].set_xlim([-.5, 3.5])
+#
+#             if cue == 0:
+#                 axs2[timewin, finger].hlines(0, axs2[timewin, finger].get_xlim()[0],
+#                                              axs2[timewin, finger].get_xlim()[1], lw=lw, color='k')
+# #
+# # for c, timewin in enumerate(timewins):
+# #     axs2[c, 0].text(1, 11, timewin)
+#
+# axs2[-1, 0].set_xticks(np.linspace(0, 3, 4))
+# axs2[-1, 0].set_xticklabels(cues)
+#
+# handles = [mpatches.Patch(color=color) for color in barcolors]
+# fig2.legend(handles=handles, labels=allFingers, loc='upper center', bbox_to_anchor=(.5, 1), ncol=5)
+#
+# fig2.supylabel('force (N)')
+# fig2.supxlabel('cued probability')
+# # fig2.tight_layout()
+# fig2.subplots_adjust(hspace=.4, right=.96, left=.1, bottom=.085, top=.9)
+# fig2.show()
--- smp0/util.py
+++ smp0/util.py
@@ -1,10 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
 # import matplotlib
 
 # matplotlib.use('MacOSX')  # Use a non-interactive backend
 
-from smp0.load_data import load_mov, load_dat, count_blocks
+from smp0.load_data import load_mov, load_dat, count_blocks, path
 
 
 def merge_blocks_mov(experiment, participant_id):
@@ -36,19 +38,19 @@ def align_force_to_stim(force, time, num_chan=5, pre_stim_time=1, post_stim_time
                               num_chan))
     NoResp = []
     for ntrial in range(num_of_trials):
-# <<<<<<< Updated upstream
+        # <<<<<<< Updated upstream
         try:
             stim_idx = np.where(time[ntrial][:, 0] > 2)[0][0]
             aligned_force[ntrial] = force[ntrial][stim_idx - fsample * pre_stim_time:
                                                   stim_idx + fsample * post_stim_time]
         except:
             NoResp.append(ntrial + 1)
-# =======
-#         print('trial: %d' % ntrial)
-#         stim_idx = np.where(time[ntrial][:, 0] > 2)[0][0]
-#         aligned_force[ntrial] = force[ntrial][stim_idx - fsample * pre_stim_time:
-#                                               stim_idx + fsample * post_stim_time]
-# >>>>>>> Stashed changes
+    # =======
+    #         print('trial: %d' % ntrial)
+    #         stim_idx = np.where(time[ntrial][:, 0] > 2)[0][0]
+    #         aligned_force[ntrial] = force[ntrial][stim_idx - fsample * pre_stim_time:
+    #                                               stim_idx + fsample * post_stim_time]
+    # >>>>>>> Stashed changes
 
     tAx = np.linspace(-pre_stim_time, post_stim_time,
                       int(fsample * (pre_stim_time + post_stim_time)))
@@ -56,81 +58,116 @@ def align_force_to_stim(force, time, num_chan=5, pre_stim_time=1, post_stim_time
     return aligned_force, tAx, NoResp
 
 
-def sort_by_probability(experiment, participant_id, stimFinger, datatype='raw'):
-    """
-
-    :type stimFinger: int
-    :param experiment: experiment you are analyzing (e.g., smp0, smp1)
-    :param participant_id: 100, 101, ...
-    :param stimFinger: stimulated finger: 91999 (index), 99919 (ring)
-    :param datatype: raw (raw force measured with sensors, 5 channels) or viz (visualized force on screen, 2 channels)
-    :return: data sorted by probability assigned to the stimulated finger
-    """
-
+def participant_dataframe(experiment, participant_id):
     dat = load_dat(experiment, participant_id)
-    indices = dat.index[dat.stimFinger == stimFinger]
-    dat = dat.loc[indices]
+    # indices = dat.index[dat.stimFinger == stimFinger]
+    # dat = dat.loc[indices]
 
-    match datatype:
-        case 'raw':
-            force, _, time = merge_blocks_mov(experiment, participant_id)
-        case 'viz':
-            _, force, time = merge_blocks_mov(experiment, participant_id)
-        case _:
-            force, time = None, None
+    # match datatype:
+    #     case 'raw':
+    rawF, vizF, time = merge_blocks_mov(experiment, participant_id)
+    # case 'viz':
+    #     _, force, time = merge_blocks_mov(experiment, participant_id)
+    # case _:
+    #     force, time = None, None
 
-    fforce = [force[i] for i in indices if i < len(force)]
-    ftime = [time[i] for i in indices if i < len(time)]
+    # fforce = [force[i] for i in indices if i < len(force)]
+    # ftime = [time[i] for i in indices if i < len(time)]
 
-# <<<<<<< Updated upstream
-    aligned_force, tAx, NoResp = align_force_to_stim(fforce, ftime)
-# =======
-#     aligned_force, tAx = align_force_to_stim(fforce, ftime)
-# >>>>>>> Stashed changes
+    # <<<<<<< Updated upstream
+    aligned_force_raw, tAx, NoResp = align_force_to_stim(rawF, time)
+    # aligned_force_viz, tAx, NoResp = align_force_to_stim(vizF, time)
+    # =======
+    #     aligned_force, tAx = align_force_to_stim(fforce, ftime)
+    # >>>>>>> Stashed changes
 
-    probCues = [[], [], [], []]
+    data_list = []
 
-    for index, (chordID, TN) in enumerate(zip(dat.chordID, dat.TN)):
-
+    for index, (chordID, TN, stimFinger) in enumerate(zip(dat.chordID, dat.TN, dat.stimFinger)):
         if not TN in NoResp:
+            data_list.append({
+                'Experiment': experiment,
+                'Participant': participant_id,
+                'Trial': TN,
+                'probCue': chordID,
+                'stimFinger': stimFinger,
+                'Thumb': list(aligned_force_raw[TN, :, 0]),
+                'Index': list(aligned_force_raw[TN, :, 1]),
+                'Middle': list(aligned_force_raw[TN, :, 2]),
+                'Ring': list(aligned_force_raw[TN, :, 3]),
+                'Pinkie': list(aligned_force_raw[TN, :, 4]),
+                'timeX': list(tAx)
+            })
 
-            match chordID:
-                case 39:
-                    probCues[3].append(aligned_force[index])
-                case 93:
-                    probCues[3].append(aligned_force[index])
-                case 12:
-                    if stimFinger == 91999:
-                        probCues[0].append(aligned_force[index])
-                    elif stimFinger == 99919:
-                        probCues[2].append(aligned_force[index])
-                case 21:
-                    if stimFinger == 91999:
-                        probCues[2].append(aligned_force[index])
-                    elif stimFinger == 99919:
-                        probCues[0].append(aligned_force[index])
-                case 44:
-                    probCues[1].append(aligned_force[index])
+    data = pd.DataFrame(data_list)
 
-    return probCues, tAx
+    return data
+
+
+def experiment_dataframe(experiment, participants_id, path=path):
+    data = pd.DataFrame()
+
+    for id in participants_id:
+        data = pd.concat(
+            [data, participant_dataframe(experiment, id)]
+        )
+
+    data.to_csv(path + experiment + '/' + experiment + '.tsv')
+
+    return data
+
+def sort_by_finger_and_probability(data, stimFinger, probCue):
+
+    indices = data.index[(data.stimFinger == stimFinger) &
+                         (data.probCue == probCue)]
+    data = data.loc[indices]
+
+
+    # probCues._append({'Experiment': experiment,
+    #                   'Participant': participant_id,
+    #                   'Trial': TN,
+    #                   'Probability Cue': chordID,
+    #                   'Stimulated Finger': stimFinger,
+    #                   'Raw Force': rawF[TN],
+    #                   'Viz Force': vizF[TN],
+    #                   'timeX': tAx})
+    #
+    # match chordID:
+    #     case 39:
+    #         probCues[3].append(aligned_force[index])
+    #     case 93:
+    #         probCues[3].append(aligned_force[index])
+    #     case 12:
+    #         if stimFinger == 91999:
+    #             probCues[0].append(aligned_force[index])
+    #         elif stimFinger == 99919:
+    #             probCues[2].append(aligned_force[index])
+    #     case 21:
+    #         if stimFinger == 91999:
+    #             probCues[2].append(aligned_force[index])
+    #         elif stimFinger == 99919:
+    #         #             probCues[0].append(aligned_force[index])
+    #         #     case 44:
+    #         #         probCues[1].append(aligned_force[index])
+    #
+    # return data
+
 
 def average_within_timewin(probCues, timewin, pre_stim_time=1, fsample=500):
-
     meanTimewin = [[[], [], [], []], [[], [], [], []]]
     for finger in range(2):
         for cue in range(len(probCues[finger][0])):
             for ntrial in range(len(probCues[finger][0][cue])):
-
-                pc = np.array(probCues[finger][0][cue][ntrial][int((pre_stim_time - timewin[0]) * fsample):int((pre_stim_time+ timewin[1]) * fsample)])
+                pc = np.array(probCues[finger][0][cue][ntrial][
+                              int((pre_stim_time - timewin[0]) * fsample):int((pre_stim_time + timewin[1]) * fsample)])
                 meanTimewin[finger][cue].append(pc.mean(axis=0))
 
             meanTimewin[finger][cue] = np.array(meanTimewin[finger][cue])
 
-
     return meanTimewin
 
-def subtract_baseline(probCues, experiment='smp0', baseline_type='clamped', block='01'):
 
+def subtract_baseline(probCues, experiment='smp0', baseline_type='clamped', block='01'):
     baseline, _, time = load_mov(experiment, baseline_type, block)
     baseline_dat = load_dat(experiment, baseline_type)
 
@@ -148,14 +185,9 @@ def subtract_baseline(probCues, experiment='smp0', baseline_type='clamped', bloc
     for finger in range(len(probCues)):
         for cue in range(len(probCues[finger][0])):
             for ntrial in range(len(probCues[finger][0][cue])):
-
                 probCues[finger][0][cue][ntrial] = probCues[finger][0][cue][ntrial] - mbaseline[finger]
 
     return probCues
-
-
-
-
 
 # def average_response_finger(experiment, participant_id, block, finger, datatype='raw', num_chan=5, plot=True, out=True):
 #     # finger: value from 1 to 5
