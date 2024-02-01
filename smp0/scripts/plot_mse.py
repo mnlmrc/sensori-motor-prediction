04017d96d92d89f2b4b61173246c6790892fd4e8
Marco Emanuele <97095997+mnlmrc@users.noreply.github.com>
2024-02-01 16:24:06 -0500

    Update plot_mse.py

--- smp0/scripts/plot_mse.py
+++ smp0/scripts/plot_mse.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     labels = ['coefficient #1 (index-like)', 'coefficient #2 (ring-like)']
     palette = {labels: color for labels, color in zip(labels, colors)}
 
-    mse = pd.DataFrame(columns=['coeff', 'participant_id', 'stimFinger', 'cue', 'timepoint', 'MSE'])
+    mse = pd.DataFrame(columns=['coeff', 'participant_id', 'stimFinger', 'cue', 'timepoint', 'Dnorm'])
     for p, participant_id in enumerate(participants):
         for tp in timepoints[1:]:
             pdata = data[(data['participant_id'] == participant_id) & (data['timepoint'] == tp)]
@@ -65,7 +65,7 @@ if __name__ == "__main__":
                                     'stimFinger': stimF,
                                     'cue': cue,
                                     'timepoint': tp,
-                                    'MSE': np.sum((Y - Yhat) ** 2) / np.sum((Y - Y.mean()) ** 2)
+                                    'Dnorm': np.sqrt(np.sum((Y - Yhat) ** 2)) / np.linalg.norm(Y, 'fro')
                                 }
                             else:
                                 pass
@@ -80,7 +80,7 @@ if __name__ == "__main__":
             if len(subset) > 0:
                 for tp in timepoints[1:]:
                     subset_tp = subset[subset['timepoint'] == tp]
-                    pw = pairwise(subset_tp, 'coeff', dep_var='MSE')
+                    pw = pairwise(subset_tp, 'coeff', dep_var='Dnorm')
                     row = {
                         'group1': pw['group1'][0],
                         'group2': pw['group2'][0],
@@ -92,7 +92,7 @@ if __name__ == "__main__":
                         'timepoint': tp
                     }
                     ttest.loc[len(ttest)] = row
-            g = sns.barplot(ax=axs[c, sF], data=subset, x='timepoint', y='MSE', hue='coeff', errorbar='se',
+            g = sns.barplot(ax=axs[c, sF], data=subset, x='timepoint', y='Dnorm', hue='coeff', errorbar='se',
                             palette=palette, legend=None, hue_order=labels, err_kws={'color': 'k'})
             axs[c, sF].set_ylabel('')
             axs[c, sF].set_xlabel('')
@@ -101,30 +101,33 @@ if __name__ == "__main__":
 
             if sF == 0:
                 axs[c, sF].spines[['top', 'bottom', 'right']].set_visible(False)
+                axs[c, sF].tick_params(bottom=False)
             elif sF == 1:
                 axs[c, sF].spines[['top', 'bottom', 'right', 'left']].set_visible(False)
                 axs[c, sF].tick_params(left=False, bottom=False)
 
-    g.set_yscale("log")
+    # g.set_yscale("log")
 
     axs[0, 0].set_title('StimFinger:index')
     axs[0, 1].set_title('StimFinger:ring')
     axs[-1, 0].spines[['bottom']].set_visible(True)
-    axs[-1, 1].spines[['bottom']].set_visible(True)
+    axs[-2, 1].spines[['bottom']].set_visible(True)
     axs[-1, 0].tick_params(bottom=True)
-    axs[-1, 1].tick_params(bottom=True)
+    axs[-2, 1].tick_params(bottom=True, labelbottom=True)
+    axs[0, 0].set_visible(False)
+    axs[-1, 1].set_visible(False)
 
     lh = [mpatches.Patch(color=color, label=label)
           for label, color in zip(labels, colors)]
     fig.legend(handles=lh, loc='upper center', ncol=2, edgecolor='none', facecolor='whitesmoke')
 
-    fig.supylabel('Mean Squared Error')
+    fig.supylabel('Normalized Euclidean Distance')
 
     fig.tight_layout()
     fig.subplots_adjust(top=.91, wspace=.17, right=.94)
 
     for c, cue in enumerate(cues):
-        fig.text(.98, np.mean((axs[c, 0].get_position().p0[1], axs[c, 0].get_position().p1[1])),
+        fig.text(.53, np.mean((axs[c, 0].get_position().p0[1], axs[c, 0].get_position().p1[1])),
                  f"probability:{cue}", va='center', ha='center', rotation=90)
 
     plt.show()
