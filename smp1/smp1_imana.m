dc0780c1be9b2023761d44cd62fe76b5509424a4
Marco Emanuele <97095997+mnlmrc@users.noreply.github.com>
2024-04-05 17:40:52 -0400

    Update smp1_imana.m

--- smp1/smp1_imana.m
+++ smp1/smp1_imana.m
@@ -930,9 +930,10 @@ function varargout = smp1_imana(what,varargin)
                 J.cvi =  'fast';
                 
             end
-
-            spm_rwls_run_fmri_spec(J);
+            
             dsave(fullfile(J.dir{1},sprintf('%s_reginfo.tsv', subj_id)), T);
+            spm_rwls_run_fmri_spec(J);
+            
             % fprintf('- estimates for glm_%d session %d has been saved for %s \n', glm, ses, subj_str{s});
 
         case 'GLM:visualize_design_matrix'
