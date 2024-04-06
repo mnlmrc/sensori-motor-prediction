c43978a0881bd09725ff694b40cf81b8b410d656
Marco <97095997+mnlmrc@users.noreply.github.com>
2024-04-05 20:35:21 -0400

    Update smp1_imana.m

--- smp1/smp1_imana.m
+++ smp1/smp1_imana.m
@@ -1074,10 +1074,10 @@ function varargout = smp1_imana(what,varargin)
                 idx = 1;
                 for ic = ucond
                     xcon = zeros(1, size(SPM.xX.X,2));
-                    xcon(:, T.(c)==ic) = 1;
+                    xcon(:, strcmp(T.(c), ic)) = 1;
                     xcon(:, T.name==baseline) = -1; 
                     xcon = xcon/abs(sum(xcon));
-                    contrast_name = sprintf('%s-%s', unique(T.name(T.(c) == ic)), baseline);
+                    contrast_name = sprintf('%s-%s', unique(T.name(strcmp(T.(c), ic))), baseline);
                     SPM.xCon(idx) = spm_FcUtil('Set', contrast_name, 'T', 'c', xcon', SPM.xX.xKXs);
                     idx = idx +1;
                 end
