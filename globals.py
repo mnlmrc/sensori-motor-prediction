b106b4f333c860dcef3a8fd1ac07f8af3b165c9d
Marco Emanuele <97095997+mnlmrc@users.noreply.github.com>
2024-04-25 15:31:00 -0400

    Update globals.py

--- globals.py
+++ globals.py
@@ -1,49 +1,21 @@
 import os
 from pathlib import Path
 
-
-# def make_dirs(experiment=None, folder=None, participant_id=None):
-#     """
-#
-#     :param experiment: experiment code (e.g., smp0, smp1, ...)
-#     :param folder: folder that contains data (e.g., behavioural, training, emg, ...)
-#     :param participant_id: participant identifier
-#     :return:
-#     """
-#
-#     directory = os.path.join(baseDir, experiment, folder, f"subj{participant_id}")
-#
-#     # if datatype is not None and participant_id.isdigit():
-#     #     directory = os.path.join(base_dir, experiment, folder, f"subj{participant_id}")
-#     # elif datatype is not None and not participant_id.isdigit():
-#     #     directory = os.path.join(base_dir, experiment, folder, f"{participant_id}")
-#     # elif datatype is None and participant_id.isdigit():
-#     #     directory = os.path.join(base_dir, experiment, folder, f"subj{participant_id}")
-#     # elif datatype is None and not participant_id.isdigit():
-#     #     directory = os.path.join(base_dir, experiment, folder, f"{participant_id}")
-#     # else:
-#     #     directory = None
-#
-#     return directory
-
-
-# base_dir = '/Users/mnlmrc/Library/CloudStorage/GoogleDrive-mnlmrc@unife.it/My Drive/UWO/SensoriMotorPrediction'
-# if not Path(base_dir).exists():
-#     base_dir = '/content/drive/My Drive/UWO/SensoriMotorPrediction/'
-#     print(base_dir)
-# else:
-#     print(base_dir)
-
-
-baseDir = "/Volumes/diedrichsen_data$/data/SensoriMotorPrediction/"
+baseDir = ["/Volumes/diedrichsen_data$/data/SensoriMotorPrediction/",
+           "/cifs/diedrichsen/data/SensoriMotorPrediction/"]
+for Dir in baseDir:
+    if Path(Dir).exists():
+        baseDir = Dir
+        break
 wbDir = "surfaceWB"
 glmDir = "glm"
 behavDir = "behavioural"
 trainDir = "training"
 imagingDir = "imaging_data"
-if not Path(baseDir).exists():
-    print("Switch to local directory")
-    baseDir = ('/Users/mnlmrc/Library/CloudStorage/'
-               'GoogleDrive-mnlmrc@unife.it/My Drive/UWO/SensoriMotorPrediction')
-    # base_dir = '/content/drive/My Drive/UWO/SensoriMotorPrediction'
+
+# if Path(baseDir).exists():
+#     print("Switch to local directory")
+#     baseDir = "/Volumes/diedrichsen_data$/data/SensoriMotorPrediction/"
+#
+#     # base_dir = '/content/drive/My Drive/UWO/SensoriMotorPrediction'
 print("Base directory:", baseDir)
