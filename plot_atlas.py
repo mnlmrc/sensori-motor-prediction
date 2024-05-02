9065d66549a85124ab75ed667c6d88ec1e7659d2
Marco <97095997+mnlmrc@users.noreply.github.com>
2024-05-01 21:59:21 -0400

    Update plot_atlas.py

--- plot_atlas.py
+++ plot_atlas.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import nibabel as nb
+import matplotlib.pyplot as plt
 
 import sys
 
@@ -25,19 +26,27 @@ if __name__ == "__main__":
     pathAtlas = os.path.join(gl.baseDir, experiment, 'atlases')
 
     Hem = ['L', 'R']
-    h = 0
-
+    hemispheres = ['Left hemisphere', 'Right hemisphere']
     surface = ['fs32k_L', 'fs32k_R']
 
-    A = nb.load(os.path.join(pathAtlas, f'{atlas}.32k.{Hem[h]}.label.gii'))
+    fig, axs = plt.subplots(1, 2, figsize=(12, 5))
 
-    surf.plot.plotmap(A.darrays[0].data, surface[h],
-                      underlay=None,
-                      borders=borders[h],
-                      cscale=[0, 4],
-                      underscale=[-1.5, 1],
-                      alpha=.5,
-                      new_figure=False,
-                      colorbar=False)
+    for h in range(2):
+        A = nb.load(os.path.join(pathAtlas, f'{atlas}.32k.{Hem[h]}.label.gii'))
+        plt.sca(axs[h])
+        surf.plot.plotmap(A.darrays[0].data, surface[h],
+                          underlay=None,
+                          cmap='tab20',
+                          borders=borders[h],
+                          underscale=[-1.5, 1],
+                          alpha=.8,
+                          new_figure=False,
+                          colorbar=False)
+        plt.title(hemispheres[h])
+
+    plt.suptitle(f'atlas:{atlas}')
+    plt.savefig(os.path.join(gl.baseDir, experiment, 'atlases',
+                             f"flat_{atlas}" + '.png'), dpi=600)
+    plt.show()
 
 
