512213649e074784be7cf8a7f73800d621f71bfc
Marco Emanuele <97095997+mnlmrc@users.noreply.github.com>
2024-06-01 19:56:28 -0400

    update

--- poster_uksm24_emg_timec_channel.py
+++ poster_uksm24_emg_timec_channel.py
@@ -26,24 +26,39 @@ if __name__ == "__main__":
                                                    'subj108',
                                                    'subj109',
                                                    'subj110'], help='')
-    parser.add_argument('--channels', default=[
-                                               'index_flex',
-                                               ], help='')
+    parser.add_argument('--channel', default='index_flex', help='')
+    parser.add_argument('--stimFinger', default='index', help='')
 
     args = parser.parse_args()
 
     experiment = args.experiment
     participant_ids = args.participants
-    channels = args.channels
+    channel = args.channel
+    stimFinger = args.stimFinger
 
     path = os.path.join(gl.baseDir, experiment)
 
     participants = pd.read_csv(os.path.join(gl.baseDir, experiment, 'participants.tsv'), sep='\t')
 
-    cue_code = [93, 12, 44, 21, 39]
-    stimFinger_code = [91999, 99919]
+    latency = pd.read_csv(os.path.join(gl.baseDir, 'smp0', 'clamped', 'smp0_clamped_latency.tsv'), sep='\t')
+    latency = latency[stimFinger].to_numpy()[0]
 
-    Dict = {ch: [] for ch in channels}
+    map_cue = {
+        '0%': 93,
+        '25%': 12,
+        '50%': 44,
+        '75%': 21,
+        '100%': 39
+    }
+
+    map_stimFinger = {
+        'index': 91999,
+        'ring': 99919
+    }
+    stimF = map_stimFinger[stimFinger]
+
+    emg_all = list()
+
     for participant in participant_ids:
 
         sn = int(''.join([c for c in participant if c.isdigit()]))
@@ -55,47 +70,36 @@ if __name__ == "__main__":
         dat = dat[dat.BN.isin(blocks)]
         ch_p = participants[participants['sn'] == sn].channels_emg.iloc[0].split(',')
 
-        for ch in Dict.keys():
-            if ch in ch_p:
-                idx = ch_p.index(ch)
-                emg_av = np.zeros((len(cue_code), len(stimFinger_code), emg.shape[-1]))
-                for sf, stimF in enumerate(stimFinger_code):
-                    for c, cue in enumerate(cue_code):
-                        emg_av[c, sf] = emg[(dat.chordID == cue) & (dat.stimFinger == stimF), idx].mean(axis=0)
+        # for ch in Dict.keys():
+        #     if ch in ch_p:
+        idx = ch_p.index(channel)
+        emg_av = np.zeros((len(map_cue.items()), emg.shape[-1]))
+        # for sf, stimF in enumerate(stimFinger_code):
+        for c, cue in enumerate(map_cue.items()):
+            emg_av[c] = emg[(dat.chordID == cue[1]) & (dat.stimFinger == stimF), idx].mean(axis=0)
 
-                Dict[ch].append(emg_av)
+        emg_all.append(emg_av)
+
+    emg_all = np.array(emg_all)
+
+    fig, axs = plt.subplots()
+
+    tAx = np.linspace(-1, 2, emg_all.shape[-1]) - latency
 
     colors = make_colors(5)
-    palette = {cue: color for cue, color in zip(['0%', '25%', '50%', '75%', '100%'], colors)}
 
-    tAx = np.linspace(-1, 2, emg.shape[-1])
+    y = np.nanmean(emg_all, axis=0)
+    for c, cue in enumerate(map_cue.items()):
+        axs.plot(tAx, y[c], label=cue[0], color=colors[c])
 
-    fig, axs = plt.subplots(len(channels), 2,
-                            sharey=True, sharex=True, figsize=(8, 10))
-
-    if axs.ndim < 2:
-        axs = np.expand_dims(axs, axis=0)
-
-    for sf, stimF in enumerate(['index', 'ring']):
-        for c, ch in enumerate(channels):
-
-            if (c == 0) & (sf == 0):
-                axs[c, sf].set_title(f'stimFinger:Index\n{ch}')
-            elif (c == 0) & (sf == 1):
-                axs[c, sf].set_title(f'stimFinger:Ring\n{ch}')
-            else:
-                axs[c, sf].set_title(ch)
-
-            subset = np.nanmean(np.array(Dict[ch]), axis=0)
-
-            for col, color in enumerate(palette):
-                axs[c, sf].plot(tAx, subset[col, sf], color=palette[color])
-
-            axs[c, sf].set_xlim([-.1, .5])
+    axs.set_title('Average EMG activity from one electrode placed over flexor digitorum '
+                  'superficialis\nwhen the perturbation was delivered to the index finger')
+    axs.set_ylabel('EMG (mV)')
+    axs.set_xlabel('time relative to stimulation (s)')
+    axs.set_xlim([-.05, .2])
+    axs.legend()
 
 
-    # fig.tight_layout()
 
-    # fig.savefig(os.path.join(gl.baseDir, experiment, 'figures', participant_id, 'force_bins.png'))
 
-    plt.show()
+
